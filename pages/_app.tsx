13f0a608f2e85bb0ee79558e983be5f837834051
elocremarc <elocremarc@gmail.com>
2021-07-25 16:08:07 -0700

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider: any): Web3Provider {
+  return new Web3Provider(provider);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Web3ReactProvider>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
